ca428eaad41224b51d6bf958de0da21d8e0ce777
Robert Bachta <erosnemesis@gmail.com>
2019-04-24 18:43:44 -0700

    fixed trafficprediction

--- src/MasterServer/TrafficPrediction.java
+++ src/MasterServer/TrafficPrediction.java
@@ -1,17 +1,10 @@
 package MasterServer;
 
 public class TrafficPrediction {
-<<<<<<< HEAD
+
     // accumulatedData is all data collected in the last 3 weeks in order to safetly predict a pattern
     private MicroController[][] acculumatedData;
     private String prediction;
-=======
-<<<<<<< HEAD
-
-}
-=======
-    /*
->>>>>>> 07d3e607b4d9eaf1a3ded5e23fb63707a7d035dc
 
     // add data through array to matrix
     public TrafficPrediction(MicroController[] newData){
@@ -23,4 +16,3 @@ public class TrafficPrediction {
         return prediction;
     }
 }
->>>>>>> 0414efa6c251bb43d5b092fc69b23637c6526665
