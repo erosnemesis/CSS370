b4495b4839454f229861652b01769e0900735893
Jeffrey Murray <jeffmur@uw.edu>
2019-04-24 18:11:20 -0700

    TrafficPrediction

--- src/MasterServer/TrafficPrediction.java
+++ src/MasterServer/TrafficPrediction.java
@@ -1,7 +1,17 @@
 package MasterServer;
 
 public class TrafficPrediction {
-    /*
+    // accumulatedData is all data collected in the last 3 weeks in order to safetly predict a pattern
+    private MicroController[][] acculumatedData;
+    private String prediction;
 
-     */
+    // add data through array to matrix
+    public TrafficPrediction(MicroController[] newData){
+        acculumatedData.add(newData);
+        System.out.println("New Data Added!")
+    }
+
+    public String getPrediction() {
+        return prediction;
+    }
 }
